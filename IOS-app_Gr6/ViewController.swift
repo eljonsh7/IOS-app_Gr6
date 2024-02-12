2f9c856226e9528a0f201f8e825d2a20d90499e0
Eljon Shala <eljon.shala@student.uni-pr.edu>
2024-02-12 23:43:27 +0100

    Initial Commit

---
+++ IOS-app_Gr6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS-app_Gr6
+//
+//  Created by Beslind Mema on 12.2.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
